db168dd62dfeea13ff5801886338f991e9166fce
nyhryan <nyhryan99102@gmail.com>
2024-09-27 14:42:37 +0900

    pino logger 추가

--- back-node/src/index.ts
+++ back-node/src/index.ts
@@ -1,14 +1,18 @@
 import express, { type Request, type Response } from "express";
 import env from "./utils/config";
+import {logger} from "./utils/logger";
 
 const server = express();
 
 server.use(express.json());
-server.use(express.urlencoded());
+server.use(express.urlencoded({ extended: true }));
 
 server.get("/", (req: Request, res: Response) => {
   res.send("Hello from back-node");
+  logger.info("This is info");
+  logger.warn("This is %s", "warning");
 });
+
 
 server.listen(parseInt(env.EXPRESS_PORT), () => {
   console.log(`Server running on http://localhost:${env.EXPRESS_PORT}`);
