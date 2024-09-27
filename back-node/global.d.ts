a8df76a1179e13dfa8c25d1c42b0400d01174c04
nyhryan <nyhryan99102@gmail.com>
2024-09-27 14:42:55 +0900

    NODE_ENV 환경변수에 대한 타입 추가

--- back-node/global.d.ts
+++ back-node/global.d.ts
@@ -1,6 +1,7 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
+      NODE_ENV: "development" | "production";
       EXPRESS_PORT: string;
       MYSQL_HOST: string | "localhost";
       MYSQL_PORT: string;
