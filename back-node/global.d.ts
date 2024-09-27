c033535c64d1cdf5cea4899009ae17853cc90fb8
nyhryan <nyhryan99102@gmail.com>
2024-09-27 15:53:10 +0900

    process.env 타입 수정 및 추가

--- back-node/global.d.ts
+++ back-node/global.d.ts
@@ -1,13 +1,14 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      NODE_ENV: "development" | "production";
+      NODE_ENV: "dev" | "production";
       EXPRESS_PORT: string;
       MYSQL_HOST: string | "localhost";
       MYSQL_PORT: string;
       MYSQL_USER: string;
       MYSQL_PASSWORD: string;
       MYSQL_DATABASE: string;
+      TEST: string;
       // Add more environment variables here...
     }
   }
