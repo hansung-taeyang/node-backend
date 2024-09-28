03f812d336dbcb83baf91070b313de4ce8945d75
nyhryan <nyhryan99102@gmail.com>
2024-09-29 02:34:57 +0900

    Swagger 문서 작성을 위한 준비 완료함. 이제 JsDoc 형태로 주석을 작성하여 Swagger 문서를 작성할 수 있음.

--- back-node/src/index.ts
+++ back-node/src/index.ts
@@ -2,10 +2,18 @@ import express, { type Request, type Response } from "express";
 import env from "./utils/config";
 import logger from "./utils/logger";
 
+import sample from "./routes/sample";
+import swagger from "./routes/swagger";
+
 const server = express();
 
+// NOTE - Middlewares
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
+
+//NOTE - Routes
+server.use("/sample", sample);
+server.use("/docs", swagger);
 
 server.get("/", (req: Request, res: Response) => {
   res.send("Hello from back-node");
@@ -13,11 +21,11 @@ server.get("/", (req: Request, res: Response) => {
   logger.warn("This is %s", "warning");
 });
 
+// All the routes that server won't accept will be redirected to 404
 server.all("*", (req: Request, res: Response) => {
   res.status(404).send("Not found");
 });
 
 server.listen(parseInt(env.EXPRESS_PORT), () => {
-  logger.info(`TEST: ${env.TEST}`);
   logger.info(`Server running on http://localhost:${env.EXPRESS_PORT}`);
 });
