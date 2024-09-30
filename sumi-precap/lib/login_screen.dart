49ab1d4551dfda718ef5e8ef264d618b07fac575
sumi-03 <lsmkoo03@naver.com>
2024-10-01 00:27:20 +0900

    로그인 화면 위젯 수정 (웹페이지상의 위치도 고려)

--- sumi-precap/lib/login_screen.dart
+++ sumi-precap/lib/login_screen.dart
@@ -5,114 +5,113 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var screenSize = MediaQuery.of(context).size;
+    var isWeb = screenSize.width > 600; // 화면 너비 600px 이상일 경우 웹으로 간주
+
     return Scaffold(
       resizeToAvoidBottomInset: true,
       backgroundColor: const Color(0xFFF2F2F2),
       body: SafeArea(
         child: SingleChildScrollView(
           padding: const EdgeInsets.fromLTRB(20.0, 150.0, 20.0, 20.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Image(
-                image: AssetImage('assets/logo.png'),
-                width: 200,
-                height: 100,
-              ),
-              const SizedBox(height: 50),
-              TextField(
-                decoration: InputDecoration(
-                  hintText: '이메일',
-                  filled: true,
-                  fillColor: Colors.white,
-
-                  border: InputBorder.none,
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(30.0),
-                    borderSide: const BorderSide(
-                        color: Colors.transparent),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(30.0),
-                    borderSide: const BorderSide(color: Colors.transparent),
-                  ),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Image(
+                  image: AssetImage('assets/logo.png'),
+                  width: 200,
+                  height: 100,
                 ),
-                keyboardType: TextInputType.emailAddress,
-              ),
-
-              const SizedBox(height: 20),
-
-              TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  hintText: '비밀번호',
-                  filled: true,
-                  fillColor: Colors.white,
-                  border: InputBorder.none,
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(30.0),
-                    borderSide: const BorderSide(
-                        color: Colors.transparent),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(30.0),
-                    borderSide: const BorderSide(color: Colors.transparent),
-                  ),
-                ),
-              ),
-
-              const SizedBox(height: 50),
-
-              SizedBox(
-                width: double.infinity,
-                height: 50,
-                child: ElevatedButton(
-                  onPressed: () {
-
-                  },
-                  style: ElevatedButton.styleFrom(
-                    foregroundColor: const Color(0xFF4157F5),
-                    backgroundColor: const Color(0xFF98A5FF),
-                    shape: RoundedRectangleBorder(
-                      borderRadius:
-                      BorderRadius.circular(50.0),
+                const SizedBox(height: 50),
+                SizedBox(
+                  width: isWeb ? 500 : double.infinity,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: '이메일',
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: InputBorder.none,
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30.0),
+                        borderSide: const BorderSide(color: Colors.transparent),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30.0),
+                        borderSide: const BorderSide(color: Colors.transparent),
+                      ),
                     ),
+                    keyboardType: TextInputType.emailAddress,
                   ),
-                  child: const Text(
-                    '로그인',
-                    style: TextStyle(
-                      fontSize: 17.0,
-                      fontWeight: FontWeight.bold,
+                ),
+                const SizedBox(height: 20),
+                SizedBox(
+                  width: isWeb ? 500 : double.infinity,
+                  child: TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: '비밀번호',
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: InputBorder.none,
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30.0),
+                        borderSide: const BorderSide(color: Colors.transparent),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30.0),
+                        borderSide: const BorderSide(color: Colors.transparent),
+                      ),
                     ),
                   ),
                 ),
-              ),
-              const SizedBox(height: 10),
-              SizedBox(
-                width: double.infinity,
-                height: 50,
-                child: ElevatedButton(
-                  onPressed: () {
-
-                  },
-                  style: ElevatedButton.styleFrom(
-                    foregroundColor: const Color(0xFF4157F5),
-                    backgroundColor: const Color(0xFFCCD2FF),
-                    shape: RoundedRectangleBorder(
-                      borderRadius:
-                      BorderRadius.circular(50.0),
+                const SizedBox(height: 50),
+                SizedBox(
+                  width: isWeb ? 500 : double.infinity,
+                  height: 50,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      foregroundColor: const Color(0xFF4157F5),
+                      backgroundColor: const Color(0xFF98A5FF),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(50.0),
+                      ),
                     ),
-                  ),
-                  child: const Text(
-                    '회원가입',
-                    style: TextStyle(
-                      fontSize: 17.0,
-                      fontWeight: FontWeight.bold,
+                    child: const Text(
+                      '로그인',
+                      style: TextStyle(
+                        fontSize: 17.0,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ],
+                const SizedBox(height: 10),
+                SizedBox(
+                  width: isWeb ? 500 : double.infinity,
+                  height: 50,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      foregroundColor: const Color(0xFF4157F5),
+                      backgroundColor: const Color(0xFFCCD2FF),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(50.0),
+                      ),
+                    ),
+                    child: const Text(
+                      '회원가입',
+                      style: TextStyle(
+                        fontSize: 17.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
