781b471541a49e7d347d9adb38b95c1402ac4f64
nyhryan <nyhryan99102@gmail.com>
2024-10-06 02:56:26 +0900

    swagger jsdoc 설정에 schema 폴더 추가

--- src/router/swagger.ts
+++ src/router/swagger.ts
@@ -12,7 +12,7 @@ const options: swaggerJsdoc.Options = {
       version: "1.0.0",
     },
   },
-  apis: ["./src/router/*.ts"],
+  apis: ["./src/router/*.ts", "./src/schema/*.ts"],
 };
 
 const specs = swaggerJsdoc(options);
