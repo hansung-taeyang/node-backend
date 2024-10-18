aa54ee3237002cd56be1d9993a1eacb724f4507c
nyhryan <nyhryan99102@gmail.com>
2024-10-19 01:37:45 +0900

    untrack drizzle folder

--- drizzle/0000_massive_vulcan.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `students` (
-	`id` serial AUTO_INCREMENT NOT NULL,
-	`name` varchar(256),
-	`year` int,
-	CONSTRAINT `students_id` PRIMARY KEY(`id`),
-	CONSTRAINT `name_idx` UNIQUE(`name`)
-);
--- drizzle/0001_faulty_jackal.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `users` (
-	`id` serial AUTO_INCREMENT NOT NULL,
-	`emailId` varchar(256),
-	`password` varchar(15),
-	`phone` varchar(11),
-	CONSTRAINT `users_id` PRIMARY KEY(`id`),
-	CONSTRAINT `email_idx` UNIQUE(`emailId`)
-);
