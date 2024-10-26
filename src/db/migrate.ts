6d30b3ce3b1d5bdbd4d58b186c41ff1fd7d9eca8
nyhryan <nyhryan99102@gmail.com>
2024-10-27 04:32:28 +0900

    Drizzle 업데이트에 따라 migrate.ts는 불필요해져서 제거

--- src/db/migrate.ts
+++
@@ -1,7 +0,0 @@
-import { migrate } from "drizzle-orm/mysql2/migrator";
-import { db, conn } from "./db";
-
-(async () => {
-    await migrate(db, { migrationsFolder: "./drizzle" });
-    conn.end();
-})();
