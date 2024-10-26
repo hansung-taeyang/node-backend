2bdf5e03cdf0d7f953c37b3cbf60efefcf521f41
sumi-03 <lsmkoo03@naver.com>
2024-10-26 19:14:10 +0900

    회원가입에서 요구하는 비밀번호 형식 변경

--- src/zod-schema/signUpSchema.ts
+++ src/zod-schema/signUpSchema.ts
@@ -8,15 +8,15 @@ export const signUpSchema = z.object({
       .min(6, { message: "Password must be at least 6 characters long" })
       .max(15, { message: "Password must not exceed 15 characters" })
       .regex(
-        /^(?=(.*[a-z]){1,})(?=(.*[A-Z]){1,})(?=(.*\d){1,})(?=(.*[#?!@$%^&*-]){1,}).*$/,
+        /^(?=(.*[a-z]){1,})(?=(.*\d){1,})(?=(.*[#?!@$%^&*-]){1,}).*$/,
         {
           message:
-            "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character",
+            "Password must contain at least one letter, one number, and one special character",
         },
       ),
     phone: z
       .string()
-      .regex(/^\d{11}$/, { message: "Phone number must be 10 digits" }),
+      .regex(/^\d{11}$/, { message: "Phone number must be 11 digits" }),
   }),
 });
 
