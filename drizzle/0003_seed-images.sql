e6d78ab76be22f6132c5599c7f4e921317471137
nyhryan <nyhryan99102@gmail.com>
2024-11-09 21:50:20 +0900

    샘플 데이터 및 이미지 추가

--- drizzle/0003_seed-images.sql
+++ drizzle/0003_seed-images.sql
@@ -6,4 +6,8 @@ VALUES
 ('sample2.jpeg', TIMESTAMP('2023-11-03'), 'foo@bar.com'),
 ('sample3.jpeg', TIMESTAMP('2023-11-04'), 'foo@bar.com'),
 ('sample4.jpeg', TIMESTAMP('2023-11-05'), 'test@abcd.com'),
-('sample5.jpeg', TIMESTAMP('2023-11-06'), 'test@abcd.com');
+('sample5.jpeg', TIMESTAMP('2023-11-06'), 'test@abcd.com'),
+('sample6.jpeg', TIMESTAMP('2023-11-07'), 'foo@bar.com'),
+('sample7.jpeg', TIMESTAMP('2023-11-08'), 'foo@bar.com'),
+('sample8.jpeg', TIMESTAMP('2023-11-09'), 'foo@bar.com'),
+('sample9.jpeg', TIMESTAMP('2023-11-10'), 'foo@bar.com');
--- drizzle/0004_seed-messages.sql
+++ drizzle/0004_seed-messages.sql
@@ -1,4 +1,64 @@
 -- Custom SQL migration file, put you code below! --
-INSERT INTO `messages` (`message_json`, `user_email_id`, `image_id`)
+INSERT INTO
+    `messages` (`message_json`, `user_email_id`, `image_id`)
 VALUES
-('{"subject": "This is title", "content": "This is content", "targetCount": "2", "targets": ["01012345678", "01098765432"]}', 'foo@bar.com', 'sample2.jpeg');
+    (
+        '{
+            "subject": "이런 책 어떠세요?",
+            "content": "해리 포터와 함께 떠나는 마법 같은 첫 모험! 영생의 비밀을 품은 ‘마법사의 돌’을 찾아 나선 해리와 친구들의 이야기를 만나보세요. 지금 바로 주문하세요!",
+            "targetCount": "2",
+            "targets": ["01012345678", "01098765432"]
+        }',
+        'foo@bar.com',
+        'sample2.jpeg'
+    ),
+    (
+        '{
+            "subject": "이런 책 어떠세요?",
+            "content": "혹시 마법 세계로의 초대장을 기다려 본 적이 있나요? 열한 살 소년 해리가 호그와트에 입학하며 시작되는 스펙터클한 모험, 해리 포터와 마법사의 돌에서 직접 확인하세요!",
+            "targetCount": "2",
+            "targets": ["01012345678", "01098765432"]
+        }',
+        'foo@bar.com',
+        'sample3.jpeg'
+    ),
+    (
+        '{
+            "subject": "이런 책 어떠세요?",
+            "content": "고아로 자란 해리, 그리고 그의 인생을 바꾼 호그와트 입학! 잊을 수 없는 모험과 우정이 가득한 해리 포터와 마법사의 돌, 지금 함께 하세요!",
+            "targetCount": "2",
+            "targets": ["01012345678", "01098765432"]
+        }',
+        'foo@bar.com',
+        'sample6.jpeg'
+    ),
+    (
+        '{
+            "subject": "이런 책 어떠세요?",
+            "content": "해리 포터와 함께 떠나는 마법 같은 첫 모험! 영생의 비밀을 품은 ‘마법사의 돌’을 찾아 나선 해리와 친구들의 이야기를 만나보세요. 지금 바로 주문하세요!",
+            "targetCount": "2",
+            "targets": ["01012345678", "01098765432"]
+        }',
+        'foo@bar.com',
+        'sample7.jpeg'
+    ),
+    (
+        '{
+            "subject": "이런 책 어떠세요?",
+            "content": "혹시 마법 세계로의 초대장을 기다려 본 적이 있나요? 열한 살 소년 해리가 호그와트에 입학하며 시작되는 스펙터클한 모험, 해리 포터와 마법사의 돌에서 직접 확인하세요!",
+            "targetCount": "2",
+            "targets": ["01012345678", "01098765432"]
+        }',
+        'foo@bar.com',
+        'sample8.jpeg'
+    ),
+    (
+        '{
+            "subject": "이런 책 어떠세요?",
+            "content": "고아로 자란 해리, 그리고 그의 인생을 바꾼 호그와트 입학! 잊을 수 없는 모험과 우정이 가득한 해리 포터와 마법사의 돌, 지금 함께 하세요!",
+            "targetCount": "2",
+            "targets": ["01012345678", "01098765432"]
+        }',
+        'foo@bar.com',
+        'sample9.jpeg'
+    );
