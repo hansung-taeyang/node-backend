78042edc241db3bb795a14edab0f0ec0f0db6e40
nyhryan <nyhryan99102@gmail.com>
2024-11-22 17:13:03 +0900

    임시 이미지 제거와 연결된 샘플 레코드 제거

--- drizzle/0003_seed-images.sql
+++ drizzle/0003_seed-images.sql
@@ -1,7 +1,6 @@
 -- Custom SQL migration file, put you code below! --
 INSERT INTO `images` (`image_id`, `created_at`, `user_email_id`)
 VALUES
-('sample0.jpeg', TIMESTAMP('2023-11-01'), 'potato@potato.com'),
 ('sample1.jpeg', TIMESTAMP('2023-11-02'), 'potato@potato.com'),
 ('sample2.jpeg', TIMESTAMP('2023-11-03'), 'foo@bar.com'),
 ('sample3.jpeg', TIMESTAMP('2023-11-04'), 'foo@bar.com'),
