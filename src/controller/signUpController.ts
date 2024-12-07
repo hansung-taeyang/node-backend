15a44564117de9ac17e0bc19fb4f1dcca4370c43
sumi <lsmkoo03@naver.com>
2024-12-07 17:37:45 +0900

    이메일 아이디 중복 확인 로직 작성 중

--- src/controller/signUpController.ts
+++ src/controller/signUpController.ts
@@ -1,4 +1,4 @@
-import { Request, Response, NextFunction } from "express";
+import e, { Request, Response, NextFunction } from "express";
 import { SignUpRequestBody } from "../zod-schema/signUpSchema";
 import { eq } from "drizzle-orm";
 import { db } from "../db/db";
@@ -6,6 +6,38 @@ import { users } from "../db/tables/users";
 import { StatusCodes } from "http-status-codes";
 
 type NewUserRecord = typeof users.$inferInsert;
+
+const idDuplicateCheckController = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { email } = req.body;
+
+  try {
+    const result = await db.query.users.findFirst({
+      where: eq(users.emailId, email),
+    });
+
+    if (result !== null) {
+      return res
+        .status(StatusCodes.CONFLICT)
+        .json({ message: "User already exists" });
+    }
+
+    const emailRegex =
+      /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*\.[a-zA-Z]{2,}$/;
+
+    if (emailRegex.test(email)) {
+      return res.status(StatusCodes.OK).json({ message: "User does not exist" });
+    }
+
+    return res.status(StatusCodes.BAD_REQUEST).json({ message: "Invalid email" });
+  } catch (error) {
+    console.error("Error in idDuplicateCheckController:", error);
+    next(error);
+  }
+};
 
 const signUpController = async (
   req: Request,
@@ -29,7 +61,7 @@ const signUpController = async (
       password: password,
       phone: phone
     };
-    
+
     await db.insert(users).values(newUser);
 
     return res.status(StatusCodes.CREATED).json({ message: "User created successfully" });
@@ -38,4 +70,4 @@ const signUpController = async (
   }
 };
 
-export default signUpController;
+export { idDuplicateCheckController, signUpController };
